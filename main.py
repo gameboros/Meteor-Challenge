92a23b2f8e349f8380fa7e414b9aa8290c22e8cf
joaovbelchi <joaovbelchi@gmail.com>
2023-05-04 20:40:03 -0300

    feat: count estrelas e meteoros

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 from PIL import Image
 
-im = Image.open("meteor_challenge_01.png")
-im.show()
-#print(im)
+#valores rgb das cores a serem analisadas
+red = (255, 0, 0, 255)
+white = (255, 255, 255, 255)
+waterBlue = (0, 0, 255, 255)
 
+#funcao que itera sob os pixels e soma nos respectivos contadores
+def count_pixels(image_path):
+    redCount, whiteCount = 0,0
+    with Image.open(image_path) as im:
+        pixels = im.load()
+        width, height = im.size
+        for y in range(height):
+            for x in range(width):
+                if pixels[x, y] == red:
+                    redCount += 1
+                elif pixels[x, y] == white:
+                    whiteCount += 1
+                    
+        return [redCount, whiteCount]
+
+count = count_pixels("meteor_challenge_01.png")
+print(f"Number of red pixels: {count[0]}, Number of white pixels: {count[1]}")
+
+#2 119 189 255 azul ceu
+#255 255 255 255 branco
+#255 0 0 255 vermelho
+#0 0 0 255 preto
+#0 0 255 255 azul agua
