4a95259573b3c31c6e23ced1c8469571460c5a0f
joaovbelchi <joaovbelchi@gmail.com>
2023-05-05 00:02:05 -0300

    feat: count meteoros na agua

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 from PIL import Image
+import bisect
 
-#valores rgb das cores a serem analisadas
+# valores rgb das cores a serem analisadas
 red = (255, 0, 0, 255)
 white = (255, 255, 255, 255)
 waterBlue = (0, 0, 255, 255)
 
-#funcao que itera sob os pixels e soma nos respectivos contadores
+# funcao que itera sob os pixels e soma nos respectivos contadores
+
+
 def count_pixels(image_path):
-    redCount, whiteCount= 0,0
+    meteors, stars = 0, 0
     waterLevel = float('inf')
+    waterLevelWidth = []
+    meteorWidths = []
     with Image.open(image_path) as im:
         pixels = im.load()
         width, height = im.size
         for y in range(height):
             for x in range(width):
                 if pixels[x, y] == red:
-                    redCount += 1
+                    meteors += 1
+                    meteorWidths.append(x)
                 elif pixels[x, y] == white:
-                    whiteCount += 1
+                    stars += 1
                 elif pixels[x, y] == waterBlue:
-                    #checa o menor valor já que estamos checando o nivel da agua inversamente
+                    # checa o menor valor já que estamos checando o nivel da agua inversamente
                     waterLevel = min(waterLevel, y)
-                    
-        return [redCount, whiteCount, waterLevel]
+                    waterLevelWidth.append(x)
 
-count = count_pixels("meteor_challenge_01.png")
-print(f"Number of red pixels: {count[0]}, Number of white pixels: {count[1]}, Water level: {count[2]}")
+        # fazendo a interseção entre as listas e ordenando de forma ascendent
+        meteorWidths = list(
+            set(meteorWidths).intersection(set(waterLevelWidth)))
+        meteorWidths.sort()
 
-#255 255 255 255 branco
-#255 0 0 255 vermelho
-#0 0 0 255 preto
-#0 0 255 255 azul agua
+        return meteors, stars, meteorWidths
+
+
+meteors, stars, meteorWidths = count_pixels(
+    "meteor_challenge_01.png")
+print(f"Number of red pixels: {meteors}, Number of white pixels: {stars}, Meteors landing on water: {meteorWidths}")
+print(meteorWidths)
+
+# 255 255 255 255 branco
+# 255 0 0 255 vermelho
+# 0 0 0 255 preto
+# 0 0 255 255 azul agua
